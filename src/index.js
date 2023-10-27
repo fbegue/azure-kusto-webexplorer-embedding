71aca62b47618701370205aaa0c9f36bf667e095
fbegue <eugene.f.begue@gmail.com>
2023-10-27 12:54:56 -0400

    init ver control (2)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { MsalProvider } from '@azure/msal-react';
 import { AuthComponent, msalInstance } from './auth'
-var pjson = require('./package.json');
+//var pjson = require('./package.json');
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const version = "0.0.1"
 root.render(
   <React.StrictMode>
-      pjson.version: {pjson.version}
+      version: {version}
       MsalProvider-start
     <MsalProvider instance={msalInstance}>
         AuthComponent-start
