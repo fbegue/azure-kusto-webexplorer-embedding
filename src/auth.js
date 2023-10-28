461696abf550b3543e3892bcc85a9d9591408618
fbegue <eugene.f.begue@gmail.com>
2023-10-27 22:40:13 -0400

    dashboard only iframe (2)

--- src/auth.js
+++ src/auth.js
@@ -8,8 +8,8 @@ import './auth.css'
 // 1. Redirect URI should be eligible for the Authorization Code Flow with PKCE (https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Authentication/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false)
 // 2. App should Grant Admin permission to Azure Date Explorer -> user_impersonation (https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/CallAnAPI/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false)
 
-const tenantId = 'd48a02e7-63fd-4c86-960a-4d76b2e9d751';
-const appId = '0289b3ad-4264-4a6c-8f0c-2c56d8177831'; // https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false
+const tenantId = '30812d17-97f9-4e0d-a1ae-69145f27a0f7';
+const appId = 'b1c5cd86-d977-498c-bee6-4ff59d170ee7'; // https://portal.azure.com/#view/Microsoft_AAD_RegisteredApps/ApplicationMenuBlade/~/Overview/appId/0289b3ad-4264-4a6c-8f0c-2c56d8177831/isMSAApp~/false
 const redirectUri = `${window.location.protocol}//${window.location.host}/blank.html`;
 
 const msalConfig = {
